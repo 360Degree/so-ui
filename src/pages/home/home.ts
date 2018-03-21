9a2d43078426da8df18f75b58d8be50682324a39
Parthiban <parthiban.g4@cognizant.com>
2018-03-21 20:22:22 +0530

    Geolocation initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,18 +1,30 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController} from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers:[Geolocation]
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController ,public geolocation:Geolocation) {
 
+    this.geolocation.getCurrentPosition().then((resp) => {
+      // resp.coords.latitude
+      // resp.coords.longitude
+     }).catch((error) => {
+       console.log('Error getting location', error);
+       
+     });
+  
   }
 
   attendance(){
     alert("success");
   }
+
+ 
 
 }
